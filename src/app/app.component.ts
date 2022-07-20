b6ae3238d1616f746cbfc7ce50c59d2aac94e370
PEDRO2221 <anonimousdex01@gmail.com>
2022-07-20 08:59:42 -0300

    Consumindo api do google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maps';
+  zoom: number = 8;
+ 
+  markers: any[] = [
+
+  ];
+  lat: number = 51.673858;
+  lng: number = 7.815982;  
+
+  mapClicked($event: any) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+
+  markerDragEnd(m: any, $event: any) {
+    console.log('dragEnd', m, $event);
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from "@agm/core";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCSPqmk39jpyxiq8nopT7zZP2HYkAHjkNw'
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
